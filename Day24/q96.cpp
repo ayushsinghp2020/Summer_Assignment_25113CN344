27566e35c62c1ca350ba2971f8fc6b4592b57058
ayushsinghp2020 <ayushsinghp2020@gmail.com>
2026-06-20 22:15:14 +0530

    Day24 done

---
+++ Day24/q96.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str;
+    
+    cout<<"Enter a string: ";
+    cin>>str;
+
+    string result="";
+
+    for(int i=0; i<str.length(); i++) {
+        bool duplicate=false;
+
+        for(int j=0; j<i; j++) {
+            if(str[i]==str[j]) {
+                duplicate=true;
+                break;
+            }
+        }
+        if(!duplicate) {
+            result += str[i];
+        }
+    }
+    cout<<"String after removing duplicates: "<<result;
+
+    return 0;
+}
